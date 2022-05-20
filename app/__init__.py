21f70d784382789344c586f8d062dc918299b81a
limu2030 <limu2030@gmail.com>
2022-05-20 18:02:39 +0300

    ft: initialize flasks extensions

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import config_options
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config.from_object(config_options[config_name])
+
+bootstrap.init_app(app)
+db.init_app(app)
